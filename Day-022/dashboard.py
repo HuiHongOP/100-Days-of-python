12253ccc94fcc844deba66119b976ac5b8b46891
Hui Hong Zheng <86531095+HuiHongOP@users.noreply.github.com>
2021-08-20 23:34:14 -0400

    Update dashboard.py

--- Day-022/dashboard.py
+++ Day-022/dashboard.py
@@ -1,1 +1,27 @@
+from turtle import Turtle
+style = ("Arial", 15, "italic")
+
+#This class helps to keep in track of the user score on top of the screen.
+class Dashboard(Turtle):
+
+    def __init__(self,position):
+        super().__init__()
+        self.score = 0
+        self.color("blue")
+        self.penup()
+        self.goto(*position)
+        self.hideturtle()
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.write(f"Score: {self.score}", align= "center", font=style)
+
+    def game_over(self,player):
+        self.goto(0, 0)
+        self.write(f"GAME OVER: {player} player has won.", align="center", font=style)
+
+    def increase_score(self):
+        self.score += 1
+        self.clear()
+        self.update_scoreboard()
 
